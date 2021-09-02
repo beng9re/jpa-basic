b0f3c9b97ba2f8986622133cd8562ba4645c3021
kbh0581 <kbh0581@gmail.com>
2021-09-02 23:38:21 +0900

    영속성 컨텍스트 구분 별 설명

--- src/main/java/jpa/JapMain.java
+++ src/main/java/jpa/JapMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JapMain {
     public static void main(String[] args) {
@@ -14,11 +15,42 @@ public class JapMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+
+//            Member member = new Member();
+//            member.setId(4L);
+//            member.setName("aaa");
+            //업데이트 쿼리 발생
+//            Member updateTagetMember =  em.find(Member.class,1L);
+//            updateTagetMember.setName("뱅스");
+//            em.remove(member);
+            // 다중 쿼리
+            List<Member> members = em.createQuery("select m from Member as m",Member.class)
+                    .setFirstResult(2)
+                    .setMaxResults(3)
+                    .getResultList();
+            //JPQL을 사용하더라도 영속성을 유지한다.
+            members.get(0).setName("키키");
+
+
+            //영속성 컨텍스트
             Member member = new Member();
-            member.setId(3L);
-            member.setName("aaa");
-            em.persist(member);
-            tx.commit();
+            member.setId(12L);
+            member.setName("비영속");
+
+            //영속 -- 디비에 저장 되지않음
+            Member findMember = em.find(Member.class,12L);
+
+
+            //비영속성 -- 디테치 이후 반영되지 않음 분리상태이기 때문
+            em.detach(member);
+            member.setName("비영속성2");
+            System.out.println("=========== 이후");
+
+
+            //저장
+
+            tx.commit(); // 커밋 시점에 db연결
 
         }catch (Exception e){
             tx.rollback();
